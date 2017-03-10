0680fecc775669ef488ec1f4f49893d2fd4c0539
nbriz <nickbriz@gmail.com>
2017-03-10 17:19:45 -0600

    finished documentation page

--- docs/make.js
+++ docs/make.js
@@ -1,7 +1,9 @@
+/* jshint esversion: 6 */
+
 const fs = require('fs');
 const cheerio = require('cheerio');
 const srcpath = "../src/elements/";
-let $; 
+let $;
 
 function cleanStr(str){
 	str = str.replace(/\t/g, '');
@@ -14,22 +16,22 @@ function parseDocComment( docs ){
 	let d = {};
 
 	for (var i = 0; i < arr.length; i++) {
-		
+
 		let str = cleanStr(arr[i]);
 
-		if( str.indexOf('name')==0 ){
+		if( str.indexOf('name')===0 ){
 			str = str.replace('name','');
 			d.name = str;
 
-		} else if( str.indexOf('description')==0 ){
+		} else if( str.indexOf('description')===0 ){
 			str = str.replace('description','');
 			d.description = str;
 
-		} else if( str.indexOf('element')==0 ){
+		} else if( str.indexOf('element')===0 ){
 			str = str.replace('element','');
 			d.element = str;
 
-		} else if( str.indexOf('attributes')==0 ){
+		} else if( str.indexOf('attributes')===0 ){
 			str = str.replace('attributes','');
 			d.attributes = JSON.parse(str);
 		}
@@ -39,24 +41,23 @@ function parseDocComment( docs ){
 }
 
 fs.readdir(srcpath,(err,files)=>{
-	if(err){ console.error(err) }
+	if(err){ console.error(err); }
 	else {
 
 		files.forEach((file)=>{
 			fs.readFile(srcpath+file,(err,data)=>{
-				if(err){ console.error(err) }
+				if(err){ console.error(err); }
 				else {
-					$ = cheerio.load(data);		
+					$ = cheerio.load(data);
 					let docComment = $('dom-module').contents()[0].next.data;
 					let docObj = parseDocComment( docComment );
 					// console.log( documentation ); // <<<<<<<<<
 					fs.writeFile(docObj.name+".json", JSON.stringify(docObj), (err)=>{
-						if(err){ console.error(err) }
-					});					
+						if(err){ console.error(err); }
+					});
 				}
-			});	
-		});		
+			});
+		});
 
 	}
 });
-
