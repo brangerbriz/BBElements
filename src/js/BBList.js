0fe587074e0505598db5a69d0b1a9ac35c9ce0a4
nbriz <nickbriz@gmail.com>
2018-07-06 19:06:26 -0500

    first commit of new version

--- src/js/BBList.js
+++
@@ -1,1 +0,0 @@
-module.exports = ['bb-cell','bb-code','bb-container','bb-div','bb-footer','bb-h1','bb-h2','bb-h3','bb-logo','bb-media','bb-note','bb-p','bb-post-summary','bb-quote','bb-row','bb-tags'];
